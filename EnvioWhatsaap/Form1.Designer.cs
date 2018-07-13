31fefb4e3b2f4abbbf50e1a44a5707532d665bb7
The Ghost <alek.ae27@gmail.com>
2018-07-13 18:52:36 -0500

    Add files via upload

---
+++ EnvioWhatsaap/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace EnvioWhatsaap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Mensaje = new System.Windows.Forms.TextBox();
+            this.Enviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Mensaje
+            // 
+            this.Mensaje.Location = new System.Drawing.Point(63, 40);
+            this.Mensaje.Multiline = true;
+            this.Mensaje.Name = "Mensaje";
+            this.Mensaje.Size = new System.Drawing.Size(594, 159);
+            this.Mensaje.TabIndex = 0;
+            // 
+            // Enviar
+            // 
+            this.Enviar.Location = new System.Drawing.Point(322, 249);
+            this.Enviar.Name = "Enviar";
+            this.Enviar.Size = new System.Drawing.Size(75, 23);
+            this.Enviar.TabIndex = 1;
+            this.Enviar.Text = "Enviar";
+            this.Enviar.UseVisualStyleBackColor = true;
+            this.Enviar.Click += new System.EventHandler(this.Enviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 306);
+            this.Controls.Add(this.Enviar);
+            this.Controls.Add(this.Mensaje);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Mensaje;
+        private System.Windows.Forms.Button Enviar;
+    }
+}
+
---
+++ EnvioWhatsaap/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace EnvioWhatsaap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Enviar_Click(object sender, EventArgs e)
+        {
+            string yourId = "";
+            string yourMobile = "+";
+            string mensaje = "Holaa"+Mensaje.Text;
+
+            try {
+
+                string url = "https://niceapi.net/API";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Headers.Add("X-APIID",yourId);
+                request.Headers.Add("X-APIMobile", yourMobile);
+                using (StreamWriter streamout = new StreamWriter(request.GetRequestStream())) {
+                    streamout.Write(mensaje);
+                }
+                using (StreamReader streamIn = new StreamReader(request.GetResponse().GetResponseStream())) {
+                    Console.WriteLine(streamIn.ReadToEnd());
+
+                }
+                Console.WriteLine("Finalizo");
+                MessageBox.Show("Finalizo el envio");
+                Mensaje.Text="";
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Termino con Exception");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
